cd963b4f455a6d5e65a861bc4478177ead1be2b0
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-14 21:13:49 -0300

    Create database class

---
+++ RelateLibrary/Database.cs
@@ -0,0 +1,8 @@
+﻿namespace RelateLibrary
+{
+    public static class Database
+    {
+        private static readonly string _connectionString =
+                @"Data Source=.\Database.db;Version=3;";
+    }
+}
