33f2c2e22e32fc22aa178a2d1420930e4e9659a2
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-15 13:32:07 -0300

    Implement entry creation

--- RelateLibrary/Database.cs
+++ RelateLibrary/Database.cs
@@ -1,8 +1,34 @@
-﻿namespace RelateLibrary
+﻿using System;
+using System.Data.SQLite;
+
+namespace RelateLibrary
 {
     public static class Database
     {
         private static readonly string _connectionString =
                 @"Data Source=.\Database.db;Version=3;";
+
+        public static void Create(Entry entry)
+        {
+            if (entry == null)
+                throw new ArgumentException
+                (
+                    "the entry to be inserted cannot be null",
+                    nameof(entry)
+                );
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = @"INSERT INTO `Entry` (`Name`) VALUES (@Name);";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    _ = command.Parameters.AddWithValue("@Name", entry.Name);
+                    _ = command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
