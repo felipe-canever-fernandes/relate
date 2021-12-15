e5fde74dc5445beebaa9ee00a56098a6bbfd49bb
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-15 13:46:54 -0300

    Implement entries reading

--- RelateLibrary/Database.cs
+++ RelateLibrary/Database.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 
 namespace RelateLibrary
@@ -29,6 +30,38 @@ namespace RelateLibrary
                     _ = command.ExecuteNonQuery();
                 }
             }
+        }
+
+        public static List<Entry> ReadEntries()
+        {
+            var entries = new List<Entry>();
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = @"SELECT * FROM `Entry`;";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    var reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                        while (reader.Read())
+                        {
+                            var entry = new Entry
+                            (
+                                reader["Name"].ToString(),
+                                int.Parse(reader["Id"].ToString())
+                            );
+
+                            entries.Add(entry);
+                        }
+
+                }
+            }
+
+            return entries;
         }
     }
 }
