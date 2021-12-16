1a8fb7be7b53df6d080514d7921a62413851f736
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-16 16:59:26 -0300

    Create relation model

---
+++ RelateLibrary/Relation.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace RelateLibrary
+{
+	public class Relation
+	{
+		private long _firstEntryId;
+		private long _secondEntryId;
+
+		public Relation(long firstEntryId, long secondEntryId)
+		{
+            FirstEntryId = firstEntryId;
+            SecondEntryId = secondEntryId;
+		}
+
+        public long FirstEntryId
+        {
+            get { return _firstEntryId; }
+
+            set
+            {
+                Debug.Assert
+                (
+                    value > 0,
+                    "The first entry ID must be positive."
+                );
+
+                _firstEntryId = value;
+            }
+        }
+
+        public long SecondEntryId
+        {
+            get { return _secondEntryId; }
+
+            set
+            {
+                Debug.Assert
+                (
+                    value > 0,
+                    "The second entry ID must be positive."
+                );
+
+                _secondEntryId = value;
+            }
+        }
+    }
+}
