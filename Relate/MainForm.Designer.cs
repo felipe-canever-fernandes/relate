a265a7bfcee3d5ddabbf60c92682e36024a3d6ae
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-17 18:44:58 -0300

    Rename main form and add title

--- Relate/MainForm.Designer.cs
+++ Relate/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Relate
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -30,13 +30,13 @@
 		{
 			this.SuspendLayout();
 			// 
-			// Form1
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Name = "MainForm";
+			this.Text = "RELATE";
 			this.ResumeLayout(false);
 
 		}
