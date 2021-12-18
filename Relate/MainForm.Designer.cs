0b0c77dd14b88a9acd6337e91ea5cbbdcddcd982
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-18 18:27:41 -0300

    Change layout

--- Relate/MainForm.Designer.cs
+++ Relate/MainForm.Designer.cs
@@ -34,8 +34,10 @@
 			this.entryLabel = new System.Windows.Forms.Label();
 			this.entryPanel = new System.Windows.Forms.Panel();
 			this.deleteButton = new System.Windows.Forms.Button();
+			this.searchPanel = new System.Windows.Forms.Panel();
 			((System.ComponentModel.ISupportInitialize)(this.entriesView)).BeginInit();
 			this.entryPanel.SuspendLayout();
+			this.searchPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// entriesView
@@ -48,14 +50,14 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.entriesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.entriesView.ColumnHeadersVisible = false;
-			this.entriesView.Location = new System.Drawing.Point(12, 40);
+			this.entriesView.Location = new System.Drawing.Point(12, 81);
 			this.entriesView.MultiSelect = false;
 			this.entriesView.Name = "entriesView";
 			this.entriesView.ReadOnly = true;
 			this.entriesView.RowHeadersVisible = false;
 			this.entriesView.RowHeadersWidth = 51;
 			this.entriesView.RowTemplate.Height = 24;
-			this.entriesView.Size = new System.Drawing.Size(258, 366);
+			this.entriesView.Size = new System.Drawing.Size(258, 360);
 			this.entriesView.TabIndex = 0;
 			this.entriesView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.entriesView_CellMouseClick);
 			// 
@@ -63,16 +65,16 @@
 			// 
 			this.filterTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.filterTextBox.Location = new System.Drawing.Point(12, 12);
+			this.filterTextBox.Location = new System.Drawing.Point(3, 3);
 			this.filterTextBox.Name = "filterTextBox";
-			this.filterTextBox.Size = new System.Drawing.Size(177, 22);
+			this.filterTextBox.Size = new System.Drawing.Size(171, 22);
 			this.filterTextBox.TabIndex = 1;
 			this.filterTextBox.TextChanged += new System.EventHandler(this.filterTextBox_TextChanged);
 			// 
 			// addButton
 			// 
 			this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.addButton.Location = new System.Drawing.Point(195, 11);
+			this.addButton.Location = new System.Drawing.Point(180, 3);
 			this.addButton.Name = "addButton";
 			this.addButton.Size = new System.Drawing.Size(75, 23);
 			this.addButton.TabIndex = 2;
@@ -91,11 +93,11 @@
 			// 
 			// entryPanel
 			// 
-			this.entryPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+			this.entryPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.entryPanel.Controls.Add(this.deleteButton);
 			this.entryPanel.Controls.Add(this.entryLabel);
-			this.entryPanel.Location = new System.Drawing.Point(12, 412);
+			this.entryPanel.Location = new System.Drawing.Point(12, 46);
 			this.entryPanel.Name = "entryPanel";
 			this.entryPanel.Size = new System.Drawing.Size(258, 29);
 			this.entryPanel.TabIndex = 4;
@@ -111,14 +113,24 @@
 			this.deleteButton.UseVisualStyleBackColor = true;
 			this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
 			// 
+			// searchPanel
+			// 
+			this.searchPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.searchPanel.Controls.Add(this.filterTextBox);
+			this.searchPanel.Controls.Add(this.addButton);
+			this.searchPanel.Location = new System.Drawing.Point(12, 12);
+			this.searchPanel.Name = "searchPanel";
+			this.searchPanel.Size = new System.Drawing.Size(258, 28);
+			this.searchPanel.TabIndex = 5;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(282, 453);
+			this.Controls.Add(this.searchPanel);
 			this.Controls.Add(this.entryPanel);
-			this.Controls.Add(this.addButton);
-			this.Controls.Add(this.filterTextBox);
 			this.Controls.Add(this.entriesView);
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -126,8 +138,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.entriesView)).EndInit();
 			this.entryPanel.ResumeLayout(false);
 			this.entryPanel.PerformLayout();
+			this.searchPanel.ResumeLayout(false);
+			this.searchPanel.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -139,6 +152,7 @@
 		private System.Windows.Forms.Label entryLabel;
 		private System.Windows.Forms.Panel entryPanel;
 		private System.Windows.Forms.Button deleteButton;
+		private System.Windows.Forms.Panel searchPanel;
 	}
 }
 
