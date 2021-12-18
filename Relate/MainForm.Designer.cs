88b8ed2a0eb387744e6906f9241a2f82a2e8e562
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-17 22:30:12 -0300

    Create entries list

--- Relate/MainForm.Designer.cs
+++ Relate/MainForm.Designer.cs
@@ -28,20 +28,46 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.entriesView = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.entriesView)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// entriesView
+			// 
+			this.entriesView.AllowUserToAddRows = false;
+			this.entriesView.AllowUserToResizeColumns = false;
+			this.entriesView.AllowUserToResizeRows = false;
+			this.entriesView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.entriesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.entriesView.ColumnHeadersVisible = false;
+			this.entriesView.Location = new System.Drawing.Point(12, 12);
+			this.entriesView.MultiSelect = false;
+			this.entriesView.Name = "entriesView";
+			this.entriesView.ReadOnly = true;
+			this.entriesView.RowHeadersVisible = false;
+			this.entriesView.RowHeadersWidth = 51;
+			this.entriesView.RowTemplate.Height = 24;
+			this.entriesView.Size = new System.Drawing.Size(776, 426);
+			this.entriesView.TabIndex = 0;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.entriesView);
 			this.Name = "MainForm";
 			this.Text = "RELATE";
+			((System.ComponentModel.ISupportInitialize)(this.entriesView)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.DataGridView entriesView;
 	}
 }
 
--- Relate/MainForm.cs
+++ Relate/MainForm.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RelateLibrary;
+using RelateLibrary.Database;
+
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Relate
 {
 	public partial class MainForm : Form
 	{
+		private BindingList<Entry> _entries = new BindingList<Entry>();
+
 		public MainForm()
 		{
 			InitializeComponent();
+
+			SetUpEntriesView();
+			ReadEntries();
+		}
+
+		private void SetUpEntriesView()
+		{
+			entriesView.DataSource = _entries;
+
+			entriesView.Columns["Id"].Visible = false;
+			entriesView.Columns["Name"].AutoSizeMode =
+				DataGridViewAutoSizeColumnMode.Fill;
+		}
+
+		private void ReadEntries()
+		{
+			_entries = new BindingList<Entry>(Database.ReadEntries());
+			entriesView.DataSource = _entries;
 		}
 	}
 }
