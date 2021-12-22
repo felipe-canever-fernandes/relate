193867ee48c8808580fd8b9c18c3ed895623a0b6
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-21 22:42:38 -0300

    Implement listing all entries on loading

--- Relate/MainForm.cs
+++ Relate/MainForm.cs
@@ -1,15 +1,34 @@
 ﻿using RelateLibrary;
+using RelateLibrary.Database;
+
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Relate
 {
 	public partial class MainForm : Form
 	{
+		private BindingList<Entry> _entries;
 		private Entry _selectedEntry;
 
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private string Filter => _filterTextBox.Text.Trim();
+		private BindingList<Entry> Entries
+		{
+			get => _entries;
+
+			set
+			{
+				Debug.Assert(value != null);
+				_entries = value;
+
+				_entriesDataGridView.DataSource = Entries;
+			}
 		}
 
 		private Entry SelectedEntry
@@ -37,6 +56,11 @@ namespace Relate
 
 		private void MainForm_Shown(object sender, System.EventArgs e)
 		{
+			Entries = new BindingList<Entry>
+			(
+				Database.ReadEntries(null, Filter)
+			);
+
 			SelectedEntry = null;
 		}
 	}
