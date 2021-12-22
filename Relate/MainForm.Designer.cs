3489d2610b95ab44dad3489499cfa5853620eb84
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-21 22:17:09 -0300

    Rename main form's components

--- Relate/MainForm.Designer.cs
+++ Relate/MainForm.Designer.cs
@@ -28,154 +28,154 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.entriesView = new System.Windows.Forms.DataGridView();
-			this.filterTextBox = new System.Windows.Forms.TextBox();
-			this.addButton = new System.Windows.Forms.Button();
-			this.entryLabel = new System.Windows.Forms.Label();
-			this.entryPanel = new System.Windows.Forms.Panel();
-			this.relatedCheckBox = new System.Windows.Forms.CheckBox();
-			this.renameButton = new System.Windows.Forms.Button();
-			this.deleteButton = new System.Windows.Forms.Button();
-			this.searchPanel = new System.Windows.Forms.Panel();
-			((System.ComponentModel.ISupportInitialize)(this.entriesView)).BeginInit();
-			this.entryPanel.SuspendLayout();
-			this.searchPanel.SuspendLayout();
+			this._entriesDataGridView = new System.Windows.Forms.DataGridView();
+			this._filterTextBox = new System.Windows.Forms.TextBox();
+			this._addButton = new System.Windows.Forms.Button();
+			this._selectedEntryNameLabel = new System.Windows.Forms.Label();
+			this._selectedEntryPanel = new System.Windows.Forms.Panel();
+			this._relatedEntriesCheckBox = new System.Windows.Forms.CheckBox();
+			this._renameSelectedEntryButton = new System.Windows.Forms.Button();
+			this._deleteSelectedEntryButton = new System.Windows.Forms.Button();
+			this._filterPanel = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this._entriesDataGridView)).BeginInit();
+			this._selectedEntryPanel.SuspendLayout();
+			this._filterPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// entriesView
+			// _entriesDataGridView
 			// 
-			this.entriesView.AllowUserToAddRows = false;
-			this.entriesView.AllowUserToResizeColumns = false;
-			this.entriesView.AllowUserToResizeRows = false;
-			this.entriesView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this._entriesDataGridView.AllowUserToAddRows = false;
+			this._entriesDataGridView.AllowUserToResizeColumns = false;
+			this._entriesDataGridView.AllowUserToResizeRows = false;
+			this._entriesDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.entriesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.entriesView.ColumnHeadersVisible = false;
-			this.entriesView.Location = new System.Drawing.Point(12, 99);
-			this.entriesView.MultiSelect = false;
-			this.entriesView.Name = "entriesView";
-			this.entriesView.ReadOnly = true;
-			this.entriesView.RowHeadersVisible = false;
-			this.entriesView.RowHeadersWidth = 51;
-			this.entriesView.RowTemplate.Height = 24;
-			this.entriesView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.entriesView.Size = new System.Drawing.Size(458, 342);
-			this.entriesView.TabIndex = 0;
+			this._entriesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this._entriesDataGridView.ColumnHeadersVisible = false;
+			this._entriesDataGridView.Location = new System.Drawing.Point(12, 99);
+			this._entriesDataGridView.MultiSelect = false;
+			this._entriesDataGridView.Name = "_entriesDataGridView";
+			this._entriesDataGridView.ReadOnly = true;
+			this._entriesDataGridView.RowHeadersVisible = false;
+			this._entriesDataGridView.RowHeadersWidth = 51;
+			this._entriesDataGridView.RowTemplate.Height = 24;
+			this._entriesDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this._entriesDataGridView.Size = new System.Drawing.Size(458, 342);
+			this._entriesDataGridView.TabIndex = 0;
 			// 
-			// filterTextBox
+			// _filterTextBox
 			// 
-			this.filterTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this._filterTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.filterTextBox.Location = new System.Drawing.Point(3, 3);
-			this.filterTextBox.Name = "filterTextBox";
-			this.filterTextBox.Size = new System.Drawing.Size(371, 22);
-			this.filterTextBox.TabIndex = 1;
+			this._filterTextBox.Location = new System.Drawing.Point(3, 3);
+			this._filterTextBox.Name = "_filterTextBox";
+			this._filterTextBox.Size = new System.Drawing.Size(371, 22);
+			this._filterTextBox.TabIndex = 1;
 			// 
-			// addButton
+			// _addButton
 			// 
-			this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.addButton.Location = new System.Drawing.Point(380, 3);
-			this.addButton.Name = "addButton";
-			this.addButton.Size = new System.Drawing.Size(75, 23);
-			this.addButton.TabIndex = 2;
-			this.addButton.Text = "Add";
-			this.addButton.UseVisualStyleBackColor = true;
+			this._addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._addButton.Location = new System.Drawing.Point(380, 3);
+			this._addButton.Name = "_addButton";
+			this._addButton.Size = new System.Drawing.Size(75, 23);
+			this._addButton.TabIndex = 2;
+			this._addButton.Text = "Add";
+			this._addButton.UseVisualStyleBackColor = true;
 			// 
-			// entryLabel
+			// _selectedEntryNameLabel
 			// 
-			this.entryLabel.AutoSize = true;
-			this.entryLabel.Location = new System.Drawing.Point(3, 6);
-			this.entryLabel.Name = "entryLabel";
-			this.entryLabel.Size = new System.Drawing.Size(37, 16);
-			this.entryLabel.TabIndex = 3;
-			this.entryLabel.Text = "Entry";
+			this._selectedEntryNameLabel.AutoSize = true;
+			this._selectedEntryNameLabel.Location = new System.Drawing.Point(3, 6);
+			this._selectedEntryNameLabel.Name = "_selectedEntryNameLabel";
+			this._selectedEntryNameLabel.Size = new System.Drawing.Size(37, 16);
+			this._selectedEntryNameLabel.TabIndex = 3;
+			this._selectedEntryNameLabel.Text = "Entry";
 			// 
-			// entryPanel
+			// _selectedEntryPanel
 			// 
-			this.entryPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this._selectedEntryPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.entryPanel.Controls.Add(this.relatedCheckBox);
-			this.entryPanel.Controls.Add(this.renameButton);
-			this.entryPanel.Controls.Add(this.deleteButton);
-			this.entryPanel.Controls.Add(this.entryLabel);
-			this.entryPanel.Location = new System.Drawing.Point(12, 46);
-			this.entryPanel.Name = "entryPanel";
-			this.entryPanel.Size = new System.Drawing.Size(458, 47);
-			this.entryPanel.TabIndex = 4;
+			this._selectedEntryPanel.Controls.Add(this._relatedEntriesCheckBox);
+			this._selectedEntryPanel.Controls.Add(this._renameSelectedEntryButton);
+			this._selectedEntryPanel.Controls.Add(this._deleteSelectedEntryButton);
+			this._selectedEntryPanel.Controls.Add(this._selectedEntryNameLabel);
+			this._selectedEntryPanel.Location = new System.Drawing.Point(12, 46);
+			this._selectedEntryPanel.Name = "_selectedEntryPanel";
+			this._selectedEntryPanel.Size = new System.Drawing.Size(458, 47);
+			this._selectedEntryPanel.TabIndex = 4;
 			// 
-			// relatedCheckBox
+			// _relatedEntriesCheckBox
 			// 
-			this.relatedCheckBox.AutoSize = true;
-			this.relatedCheckBox.Location = new System.Drawing.Point(6, 25);
-			this.relatedCheckBox.Name = "relatedCheckBox";
-			this.relatedCheckBox.Size = new System.Drawing.Size(77, 20);
-			this.relatedCheckBox.TabIndex = 5;
-			this.relatedCheckBox.Text = "Related";
-			this.relatedCheckBox.UseVisualStyleBackColor = true;
+			this._relatedEntriesCheckBox.AutoSize = true;
+			this._relatedEntriesCheckBox.Location = new System.Drawing.Point(6, 25);
+			this._relatedEntriesCheckBox.Name = "_relatedEntriesCheckBox";
+			this._relatedEntriesCheckBox.Size = new System.Drawing.Size(77, 20);
+			this._relatedEntriesCheckBox.TabIndex = 5;
+			this._relatedEntriesCheckBox.Text = "Related";
+			this._relatedEntriesCheckBox.UseVisualStyleBackColor = true;
 			// 
-			// renameButton
+			// _renameSelectedEntryButton
 			// 
-			this.renameButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.renameButton.Location = new System.Drawing.Point(299, 3);
-			this.renameButton.Name = "renameButton";
-			this.renameButton.Size = new System.Drawing.Size(75, 23);
-			this.renameButton.TabIndex = 4;
-			this.renameButton.Text = "Rename";
-			this.renameButton.UseVisualStyleBackColor = true;
+			this._renameSelectedEntryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._renameSelectedEntryButton.Location = new System.Drawing.Point(299, 3);
+			this._renameSelectedEntryButton.Name = "_renameSelectedEntryButton";
+			this._renameSelectedEntryButton.Size = new System.Drawing.Size(75, 23);
+			this._renameSelectedEntryButton.TabIndex = 4;
+			this._renameSelectedEntryButton.Text = "Rename";
+			this._renameSelectedEntryButton.UseVisualStyleBackColor = true;
 			// 
-			// deleteButton
+			// _deleteSelectedEntryButton
 			// 
-			this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.deleteButton.Location = new System.Drawing.Point(380, 3);
-			this.deleteButton.Name = "deleteButton";
-			this.deleteButton.Size = new System.Drawing.Size(75, 23);
-			this.deleteButton.TabIndex = 4;
-			this.deleteButton.Text = "Delete";
-			this.deleteButton.UseVisualStyleBackColor = true;
+			this._deleteSelectedEntryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._deleteSelectedEntryButton.Location = new System.Drawing.Point(380, 3);
+			this._deleteSelectedEntryButton.Name = "_deleteSelectedEntryButton";
+			this._deleteSelectedEntryButton.Size = new System.Drawing.Size(75, 23);
+			this._deleteSelectedEntryButton.TabIndex = 4;
+			this._deleteSelectedEntryButton.Text = "Delete";
+			this._deleteSelectedEntryButton.UseVisualStyleBackColor = true;
 			// 
-			// searchPanel
+			// _filterPanel
 			// 
-			this.searchPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this._filterPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.searchPanel.Controls.Add(this.filterTextBox);
-			this.searchPanel.Controls.Add(this.addButton);
-			this.searchPanel.Location = new System.Drawing.Point(12, 12);
-			this.searchPanel.Name = "searchPanel";
-			this.searchPanel.Size = new System.Drawing.Size(458, 28);
-			this.searchPanel.TabIndex = 5;
+			this._filterPanel.Controls.Add(this._filterTextBox);
+			this._filterPanel.Controls.Add(this._addButton);
+			this._filterPanel.Location = new System.Drawing.Point(12, 12);
+			this._filterPanel.Name = "_filterPanel";
+			this._filterPanel.Size = new System.Drawing.Size(458, 28);
+			this._filterPanel.TabIndex = 5;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(482, 453);
-			this.Controls.Add(this.searchPanel);
-			this.Controls.Add(this.entryPanel);
-			this.Controls.Add(this.entriesView);
+			this.Controls.Add(this._filterPanel);
+			this.Controls.Add(this._selectedEntryPanel);
+			this.Controls.Add(this._entriesDataGridView);
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "RELATE";
-			((System.ComponentModel.ISupportInitialize)(this.entriesView)).EndInit();
-			this.entryPanel.ResumeLayout(false);
-			this.entryPanel.PerformLayout();
-			this.searchPanel.ResumeLayout(false);
-			this.searchPanel.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this._entriesDataGridView)).EndInit();
+			this._selectedEntryPanel.ResumeLayout(false);
+			this._selectedEntryPanel.PerformLayout();
+			this._filterPanel.ResumeLayout(false);
+			this._filterPanel.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.DataGridView entriesView;
-		private System.Windows.Forms.TextBox filterTextBox;
-		private System.Windows.Forms.Button addButton;
-		private System.Windows.Forms.Label entryLabel;
-		private System.Windows.Forms.Panel entryPanel;
-		private System.Windows.Forms.Button deleteButton;
-		private System.Windows.Forms.Panel searchPanel;
-		private System.Windows.Forms.Button renameButton;
-		private System.Windows.Forms.CheckBox relatedCheckBox;
+		private System.Windows.Forms.DataGridView _entriesDataGridView;
+		private System.Windows.Forms.TextBox _filterTextBox;
+		private System.Windows.Forms.Button _addButton;
+		private System.Windows.Forms.Label _selectedEntryNameLabel;
+		private System.Windows.Forms.Panel _selectedEntryPanel;
+		private System.Windows.Forms.Button _deleteSelectedEntryButton;
+		private System.Windows.Forms.Panel _filterPanel;
+		private System.Windows.Forms.Button _renameSelectedEntryButton;
+		private System.Windows.Forms.CheckBox _relatedEntriesCheckBox;
 	}
 }
 
