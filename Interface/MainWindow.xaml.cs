4111db36c15f3166034a23a78d232996bb6d6193
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-29 17:49:03 -0300

    Implement displaying entries

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Core.Database;
+using Core.Models;
+
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Interface
 {
@@ -19,9 +11,16 @@ namespace Interface
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Entry> Entries { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            Entries =
+                new ObservableCollection<Entry>(Database.GetAllEntries());
         }
     }
 }
