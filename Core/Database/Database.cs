1cb3beb62b813fe99b0768f6827730707b6bb757
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-12-29 17:35:38 -0300

    Implement getting all entries from database

--- Core/Database/Database.cs
+++ Core/Database/Database.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Core.Models;
+
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
 
 namespace Core.Database
 {
@@ -8,5 +12,42 @@ namespace Core.Database
 				ConfigurationManager
 					.ConnectionStrings["SQLite"]
 					.ConnectionString;
+
+		public static List<Entry> GetAllEntries()
+		{
+			using (var connection = new SQLiteConnection(ConnectionString))
+			{
+				connection.Open();
+
+				var query = "PRAGMA foreign_keys = 1;";
+
+				using (var command = new SQLiteCommand(query, connection))
+				{
+					_ = command.ExecuteNonQuery();
+				}
+
+				query = "SELECT `Id`, `Name` FROM `Entry`;";
+
+				using (var command = new SQLiteCommand(query, connection))
+				{
+					using (var reader = command.ExecuteReader())
+					{
+						var entries = new List<Entry>();
+
+						while (reader.Read())
+						{
+							var entry = new Entry(
+								reader["Name"].ToString(),
+								long.Parse(reader["Id"].ToString())
+							);
+
+							entries.Add(entry);
+						}
+
+						return entries;
+					}
+				}
+			}
+		}
 	}
 }
